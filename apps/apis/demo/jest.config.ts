5a198f0048113be9be2833e256a0d0ba82caec41
Alex Berriman <alex.berriman@clearscore.com>
2023-03-30 12:35:30 +1100

    refactor: tweak project structure

--- apps/apis/demo/jest.config.ts
+++ apps/apis/demo/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'demo-api',
-  preset: '../../jest.preset.js',
+  displayName: 'api-demo',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/demo-api',
+  coverageDirectory: '../../../coverage/apps/api-demo',
 };
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/ping-pong-webapp',
+  cacheDir: '../../node_modules/.vite/web',
 
   server: {
     port: 4200,
